9ab443c56571bccccee84bb2f3dbc06fd8a19977
Nayera Hazem <69148381+nayera540@users.noreply.github.com>
2023-07-18 15:04:04 +0300

    update

---
+++ Week4/Assignment1/Q1.js
@@ -0,0 +1,10 @@
+console.log(Math.round(100000.1)); // 100000
+console.log(Math.floor(100000.2)); // 100000
+console.log(Math.ceil(99999.9)); // 100000
+console.log(100 * 1000); // 100000
+console.log(1e5); // 100000
+console.log(Number.parseInt("100000 Nano")); // 100000
+console.log(Math.pow(10,5)); // 100000,5
+console.log(+"100000"); // 100000
+console.log(Number("100000")); // 100000
+console.log(Math.sqrt(10**10)); // 100000
