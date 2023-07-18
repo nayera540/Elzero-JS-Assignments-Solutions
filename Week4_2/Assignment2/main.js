7ffb6ea0e5eaf35b9282424b3fb6d323d857fab9
Nayera Hazem <69148381+nayera540@users.noreply.github.com>
2023-07-18 19:30:49 +0300

    Update

---
+++ Week4_2/Assignment2/main.js
@@ -0,0 +1,8 @@
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes(letterZ)); // True
+console.log(word.toLowerCase().startsWith(letterE)); // True
+console.log(word.endsWith(letterO.toLowerCase())); // True
