95553b30ea43dfbc7c86c323621efdcda3c28036
Nayera Hazem <69148381+nayera540@users.noreply.github.com>
2023-07-22 23:04:38 +0300

    Week6 update

---
+++ Week6/Q1.js
@@ -0,0 +1,9 @@
+let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
+let num = 3;
+
+// Method 1
+console.log(myFriends.slice(false,num)); // ["Ahmed", "Elham", "Osama"];
+
+myFriends.pop();
+// Method 2
+console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
